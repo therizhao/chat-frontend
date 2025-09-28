7ad4c163b87280662933fee1169125cdc6f306b5
rizhaow <rizhaow@gmail.com>
2025-09-29 00:09:49 +0800

    x

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
 export const BACKEND_URL =
-  'https://chat-backend-production-a7da.up.railway.app/';
+  'https://chat-backend-production-a7da.up.railway.app';
 
 const SUPABASE_URL = 'https://piuzwrnjlllzzokawbtd.supabase.co';
 const SUPABASE_ANON_KEY = 'sb_publishable_QmHf1W4fjAKaGMQ1w5E9wA_KNrG_sEx';
